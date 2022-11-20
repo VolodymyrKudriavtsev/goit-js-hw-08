91ed851624250db64d01e38524ead83bbf18b36a
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-11-20 02:38:45 +0200

    task-02 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const player = new Player('vimeo-player', {
+  id: 'vimeo-player',
+});
+
+const getCurrentTime = e => {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime);
